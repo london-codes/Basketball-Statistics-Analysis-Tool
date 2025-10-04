95a823438f7c8f3013731db735133305612242c3
london-codes <london.powell.work@gmail.com>
2025-10-03 23:56:30 -0700

    Update README and GUI module

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
+# Basketball-Statistics-Analysis-Tool
+
 A Python-based wnba analytics tool for scraping, cleaning, analyzing, and visualizing WNBA player statistics.  
 The program automates data collection from [Basketball Reference](https://www.basketball-reference.com), stores results in CSV files. 
 
 
-not finished README
+# Usage
+- To run GUI simply run main.py module
+- If you want to update stats from the regular season run 
+list_players_update.py module.
+
+
+git clone https://github.com/london-codes/Basketball-Statistics-Analysis-Tool.git
+cd Basketball-Statistics-Analysis-Tool
+pip install -r requirements.txt
+
+
+# Limitations
+- Cannot update all players in the database before running the GUI because of Basketball reference rate limit, as it would significantly increase the time to open the application. Instead of this, User must run the list_player_update.py module to update all the players in the database.
+
+
--- src/list_players_update.py
+++ src/list_players_update.py
@@ -1,21 +1,17 @@
 from make_csv import update_player_csv
 
+
 """
 Module for updating or creating player CSVs from Basketball Reference.
 
 Usage:
-    - Use update_player_csv(url, player_name) to create a new player CSV 
-      or update an existing one with stats from the specic year game log page on the basketball-refernce.
-    - Example:
-        update_player_csv(
-            "https://www.basketball-reference.com/wnba/players/w/willico01w/gamelog/2025/",
-            "courtney"
-        )
+    - Use update_player_csv(url, player_name) to update an existing csv file with the name inputed or add a 
+    player not already in the data base.
+    - To add a player you need to navigate to basketball reference and find the gamelog page with the specifc year
+    you want.
 
 Important Notes:
-    - Basketball Reference enforces rate limits.
-    - The function includes a built-in timer to avoid requesting too quickly.
-    - About 5 players at one time seems to work fine.
+    - 5 second limit between each call of the update_player_csv function to avoid the rate limit and being blocked.
 
 """
     #   updates (commented out to avoid rate limits uncoment carefully to avoid rate limits):
